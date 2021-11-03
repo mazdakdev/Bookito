881c7a0fa491e9c8aab2f6aa58f4323dbed618b4
Mazdak Pakaghideh <mazdak.software@gmail.com>
2021-11-03 11:30:25 +0330

    Added navigation bar for Mobile devices

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,8 @@
             <a href="#responsive-header" class="  inline-block mt-0 sm:text-3xl   lg:text-xl text-white ml-14 ">
               Books
             </a>
+
+
           </div>
         </div>
       </nav>
@@ -35,7 +37,7 @@
               </div>
 
               <div class="flex-1">
-                <p class=" text-white text-sm sm:text-xl">
+                <p class=" text-white text-sm sm:text-xl xs:text-xl">
                   welcome to your own book library all the books are organized
                   <br />
                   just the way you like
@@ -43,20 +45,41 @@
               </div>
 
               <div class="flex-1 ">
-                <nuxt-link to="#" class="bg-white text-black   sm:text-2xl lg:text-xl py-4 px-8 xs:py-2 xs:px-4  rounded-full ">
+                <nuxt-link to="#" class="bg-white text-black   sm:text-2xl lg:text-xl lg:py-4 lg:px-7 py-4 px-8 xs:py-4 xs:px-7  rounded-full ">
                  All the books
                 </nuxt-link>
               </div>
             </div>
           </div>
           <div class="flex-1 ">
-            <img id="book" src="../assets/header-img.png" class="book position-absolute zoomInDown sm:mt-64 lg:w-3/5 lg:ml-48 lg:mt-5  xs:mt-32  "  alt="Book" />
+            <img id="book" src="../assets/header-img.png" class="book position-absolute zoomInDown sm:mt-64 lg:w-3/5 lg:ml-48 lg:mt-5  xs:mt-32 "  alt="Book" />
           </div>
         </div>
       </section>
       <!-- END main of header -->
 
-    </header>
+      <!-- START of Mobile nav -->
+
+      <div class="xs:flex items-center  w-10/12 rounded-full flex-wrap bg-teal-500 p-6 lg:hidden sm:hidden md:hidden absolute bottom-3 left-8 bg-purple-800  ">
+        <div class="flex-grow flex items-center w-auto ">
+          <div class="flex-grow  ">
+
+            <a href="#responsive-header" class=" inline-block mt-0 xs:text-2xl   text-white ml-10 ">
+              Home
+            </a>
+
+            <a href="#responsive-header" class="  inline-block mt-0 xs:text-2xl   text-white ml-14 ">
+              Books
+            </a>
+
+          </div>
+        </div>
+      </div>
+      <!-- END -->
+
+  </header>
+     
+  
   </div>
 </template>
 <script>
